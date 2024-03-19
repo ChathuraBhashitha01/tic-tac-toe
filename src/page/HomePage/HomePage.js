e00ae2ab6896170a225a9e3f868926708f61b2c8
MSI <chathurabhashitha01@gmail.com>
2024-03-20 02:22:20 +0530

    front-end update

--- src/page/HomePage/HomePage.js
+++ src/page/HomePage/HomePage.js
@@ -1,49 +1,29 @@
 import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import './style.css'
 
 export default function HomePage() {
-    const [spacing, setSpacing] = React.useState(2);
-
-    const jsx = `
-  <Grid container spacing={${spacing}}>
-  `;
-  
+   
     return (
-        <div className='background'>
-        <h1 className='txtGameName'>Tic Tac Toe</h1>
-      <Grid sx={{ flexGrow: 1 }} container spacing={2}>
-        <Grid item xs={12}>
-          <Grid className='firstGrid' container justifyContent="center" spacing={spacing}>
-            {[0, 1, 2].map((value) => (
-              <Grid key={value} item>
-                <button className='btnClick'/>
-              </Grid>
-            ))}
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container justifyContent="center" spacing={spacing}>
-            {[0, 1, 2].map((value) => (
-              <Grid key={value} item>
-                <button className='btnClick'/>
-              </Grid>
-            ))}
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container justifyContent="center" spacing={spacing}>
-            {[0, 1, 2].map((value) => (
-              <Grid key={value} item>
-                <button className='btnClick'/>
-              </Grid>
-            ))}
-          </Grid>
-        </Grid>
-        
-  
-      </Grid>
-      </div>
+       <div className='container'>
+         <h1 className='title'>Tic Tac Toe <span>React</span> </h1>
+         <div className='board'>
+            <div className='row1'>
+             <div className='boxes'></div>
+             <div className='boxes'></div>
+             <div className='boxes'></div>
+            </div>
+            <div className='row2'>
+             <div className='boxes'></div>
+             <div className='boxes'></div>
+             <div className='boxes'></div>
+            </div>
+            <div className='row3'>
+             <div className='boxes'></div>
+             <div className='boxes'></div>
+             <div className='boxes'></div>
+            </div>
+         </div>
+         <button className='btnRestart'>Restart</button>
+       </div>
     );
 }
